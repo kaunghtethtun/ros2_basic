1349888882dca8730f8f425edf8c2cc9cac51918
kaunghtethtun <kaunghtethtun2013@gmail.com>
2025-02-16 21:51:58 +0630

    update

---
+++ begin/launch/param3.launch.py
@@ -0,0 +1,13 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            executable='param',
+            package='custom',
+            output='screen',
+            emulate_tty=False,
+            parameters=[{'param':3}],
+        ),
+    ])
